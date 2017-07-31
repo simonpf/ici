efb81e9ff9bcc98f51cac8a453b396a0f7b2df7a
simonpf <simonpf@chalmers.se>
2017-07-31 16:25:08 +0200

    wip

--- src/toy_problem.py
+++ src/toy_problem.py
@@ -24,28 +24,36 @@ def scatter_plot():
     plt.ylabel(r"$y$")
 
 def load_data():
-    x_train = np.load("/home/simonpf/projects/ici/data/sets/toy/x_train.npy")
-    y_train = np.load("/home/simonpf/projects/ici/data/sets/toy/y_train.npy")
-    x_test = np.load("/home/simonpf/projects/ici/data/sets/toy/x_test.npy")
-    y_test = np.load("/home/simonpf/projects/ici/data/sets/toy/y_test.npy")
-    mu_test = np.load("/home/simonpf/projects/ici/data/sets/toy/mu_test.npy")
-    var_test = np.load("/home/simonpf/projects/ici/data/sets/toy/var_test.npy")
+    x_train  = np.load("../data/sets/toy/x_train.npy")
+    y_train  = np.load("../data/sets/toy/y_train.npy")
+    x_test   = np.load("../data/sets/toy/x_test.npy")
+    y_test   = np.load("../data/sets/toy/y_test.npy")
+    mu_test  = np.load("../data/sets/toy/mu_test.npy")
+    var_test = np.load("../data/sets/toy/var_test.npy")
     return (x_train, y_train, x_test, y_test, mu_test, var_test)
 
 def generate_data():
-    x_train  = np.reshape(np.random.uniform(0.0, 2.0 * np.pi, (1e5)), (-1,1))
+    x_train  = np.reshape(np.random.uniform(0.0, 2.0 * np.pi, 100000), (-1,1))
     y_train  = np.random.normal(np.sin(x_train), 0.1 + np.sin(0.5 * x_train)).ravel()
-    x_test   = np.reshape(np.random.uniform(0.0, 2.0 * np.pi, (1e4)), (-1, 1))
+    x_test   = np.reshape(np.random.uniform(0.0, 2.0 * np.pi, 10000), (-1, 1))
     y_test   = np.random.normal(np.sin(x_test), 0.1 + np.sin(0.5 * x_test)).ravel()
     mu_test  = np.sin(x_test)
     var_test = 0.1 + np.sin(0.5 * x_test)
 
-    np.save("/home/simonpf/projects/ici/data/sets/toy/x_train.npy", x_train)
-    np.save("/home/simonpf/projects/ici/data/sets/toy/y_train.npy", y_train)
-    np.save("/home/simonpf/projects/ici/data/sets/toy/x_test.npy", x_test)
-    np.save("/home/simonpf/projects/ici/data/sets/toy/y_test.npy", y_test)
-    np.save("/home/simonpf/projects/ici/data/sets/toy/mu_test.npy", mu_test)
-    np.save("/home/simonpf/projects/ici/data/sets/toy/var_test.npy", var_test)
+    np.save("../data/sets/toy/x_train.npy", x_train)
+    np.save("../data/sets/toy/y_train.npy", y_train)
+    np.save("../data/sets/toy/x_test.npy", x_test)
+    np.save("../data/sets/toy/y_test.npy", y_test)
+    np.save("../data/sets/toy/mu_test.npy", mu_test)
+    np.save("../data/sets/toy/var_test.npy", var_test)
+
+def plot_truth(ax):
+    x = np.linspace(0.0, 2.0 * np.pi, 101)
+    y = np.sin(x)
+    s = 0.1 + np.sin(0.5 * x)
+    ax.plot(x, y, c='k', lw=1)
+    ax.plot(x, y + 1.96 * s, c='k', ls="--")
+    ax.plot(x, y - 1.96 * s, c='k', ls="--")
 
 def plot_results(x_test, y_pred):
     x = np.linspace(0.0, 2.0 * np.pi, 101)
@@ -53,5 +61,4 @@ def plot_results(x_test, y_pred):
     s = 0.1 + np.sin(0.5 * x)
     plt.plot(x, y, c='k', lw=1)
     plt.scatter(x_test, y_pred, c='r', s=1)
-
 plt.show()
